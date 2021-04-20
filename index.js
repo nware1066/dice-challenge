d272fad25e18cc04086253e8e76e596ba064dbab
Naomi Ware <nware1066@yahoo.com>
2021-04-19 22:12:03 -0600

    Add conditional statement to display winner

--- index.js
+++ index.js
@@ -9,7 +9,17 @@ image1.setAttribute("src", randomImageSrc);
 
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-let randomImageSrc2 = `images/dice${randomNumber2}.png`
+let randomImageSrc2 = `images/dice${randomNumber2}.png`;
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSrc2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player One Wins!🚩";
+}
+if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player Two Wins!";
+}
+if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
 
