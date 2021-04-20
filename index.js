e6d179eebb78b9bab596964607f533d37a3aa8e1
Naomi Ware <nware1066@yahoo.com>
2021-04-19 21:55:00 -0600

    Add random number generator and random image for second die

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ let image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSrc);
 
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let randomImageSrc2 = `images/dice${randomNumber2}.png`
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSrc2);
 
